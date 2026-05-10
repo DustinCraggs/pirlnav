fee8e533613c85ee742f2d02ac508c8c4fc95ed6
Dustin <dustininc@live.com>
2026-05-10 19:49:05 +0930

    Config tweaks

--- scripts/gen_data.sh
+++ scripts/gen_data.sh
@@ -15,12 +15,13 @@ python run.py \
     --run-type gen \
     --exp-config configs/experiments/il_objectnav.yaml \
     TASK_CONFIG.DATASET.DATA_PATH $bc_dataset_path \
-    NUM_ENVIRONMENTS 10 \
+    NUM_ENVIRONMENTS 8 \
     TASK_CONFIG.DATASET.SUB_SPLIT_INDEX_PATH $sub_split_path \
     TASK_CONFIG.REPRESENTATION_GENERATOR.data_storage.output_path $output_dir \
     TASK_CONFIG.REPRESENTATION_GENERATOR.skip_look_actions False \
     TASK_CONFIG.REPRESENTATION_GENERATOR.generate_skip_index False \
-    TASK_CONFIG.DATASET.FILTER_EXISTING_PATH split_0_work_splits/completed_eps_1.txt \
+    TASK_CONFIG.DATASET.FILTER_EXISTING_PATH completed_eps_splits/edge_fix_running_splits/completed_eps_0.txt \
+    # TASK_CONFIG.DATASET.FILTER_EXISTING_PATH split_0_work_splits/completed_eps_0.txt \
     # TASK_CONFIG.DATASET.FILTER_EXISTING_PATH /home/dc/data/nav_datasets/pirlnav_costmap_datasets/ten_percent/split_0/costmap_costdist_zarr_edge_fix/completed_eps.txt \
     # TASK_CONFIG.DATASET.FILTER_EXISTING_PATH /home/dc/data/nav_datasets/pirlnav_costmap_datasets/ten_percent/split_0/costmap_zarr/segmaster_1_1_20260102_165219_epoch_149/completed_eps.txt \
     # TASK_CONFIG.DATASET.FILTER_EXISTING_PATH /storage/dc/nav_datasets/pirlnav_costmap_datasets/ten_percent/split_0/pred_costmaps/splg_full_costmaps_1_2/completed_eps.txt \
